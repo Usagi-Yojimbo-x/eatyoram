a18ffbe8b54d1c0ee45540b0df89634141a1bd90
Usagi-Yojimbo-x <69713892+Usagi-Yojimbo-x@users.noreply.github.com>
2020-12-20 21:31:34 +0630

    Create eatyoram.cpp

---
+++ eatyoram.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+int main(int argc, char** argv) {
+    int max = -1;
+    int mb = 0;
+    char* buffer;
+    int esc;
+
+    if(argc > 1)
+        max = atoi(argv[1]);
+
+/* allocating memory on heap by malloc(10 MB) and use memset to fill those allocated memory */
+    while((buffer=(char*)malloc(1024*1024*10)) != NULL && mb != max) {
+        memset(buffer, 0, 1024*1024*10);
+        mb += 10;
+        printf("Allocated %d MB\n", mb);
+    }
+    
+    printf("Press enter to exit");
+    if (esc == getchar())
+	free(buffer);
+	exit(0); 
+    return 0;
+}
