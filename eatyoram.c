89b61a0dd07a1685a38f559581b23d66829f3d5b
Usagi-Yojimbo-x <69713892+Usagi-Yojimbo-x@users.noreply.github.com>
2020-12-20 15:38:28 +0630

    Update eatyoram.c

--- eatyoram.c
+++ eatyoram.c
@@ -3,15 +3,16 @@
 #include <string.h>
 
 int main(int argc, char** argv) {
-    int max = -1;      // if there is no argument, this will make infinity loop till you run out of memory
-    int mb = 0;
+    int max = -1;      // if there is no argument, this will make 
+    int mb = 0;	       // infinity loop till you run out of memory
     char* buffer;
     int esc;
 
     if(argc > 1)
         max = atoi(argv[1]);
 
-/* allocating memory on heap by malloc(10 MB) and use memset to fill those allocated memory */
+/* allocating memory on heap by malloc(10 MB)
+and use memset to fill those allocated memory */
     while((buffer=malloc(1024*1024*10)) != NULL && mb != max) {
         memset(buffer, 0, 1024*1024*10);
         mb += 10;
@@ -19,7 +20,9 @@ int main(int argc, char** argv) {
     }
     
     printf("Press enter to exit");
-    if (esc = getchar())
+    if (esc = getchar()){
+	free(buffer);
 	exit(0);
+    }
     return 0;
 }
